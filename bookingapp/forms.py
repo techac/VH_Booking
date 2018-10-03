6bc902c047e0963390a942697088d8c747d895eb
anmol <achugh519@mail.com>
2018-10-03 21:53:08 +0530

    fix #4 added date field to forms.py

--- bookingapp/forms.py
+++ bookingapp/forms.py
@@ -25,4 +25,4 @@ class myregistrationform(UserCreationForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model=Post
-        fields={'name','check_in','check_out'}
+        fields={'name','check_in','check_out','date'}
