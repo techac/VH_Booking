633a11d245828a43ab3e454441cd2e5da9e9ddae
Adarsh Pathak <pathak.adarsh@gmail.com>
2018-10-04 09:31:08 +0530

    Fixes #4: Adding automatic current date input.

--- bookingapp/models.py
+++ bookingapp/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     name=models.TextField(max_length=100)
     check_in=models.DateField(null=True)
     check_out=models.DateField(null=True)
-    date=models.DateField(null=True, blank=True)
+    date=models.DateField(blank=True, auto_now_add=True)
 
     def __str__(self):
         return self.name
